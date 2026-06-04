f9d4330727e2923fb13a20c49f7183b0331c471b
gioitrongxuan <gioi.trongxuan@gmail.com>
2026-06-05 08:17:53 +0900

    Initial Commit

---
+++ Mai/MaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaiApp.swift
+//  Mai
+//
+//  Created by Gioi on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MaiTests/MaiTests.swift
@@ -0,0 +1,17 @@
+//
+//  MaiTests.swift
+//  MaiTests
+//
+//  Created by Gioi on 5/6/26.
+//
+
+import Testing
+@testable import Mai
+
+struct MaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
